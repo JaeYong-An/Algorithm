f51a43a3635ae854164647297ddcbf9ff6b378e7
JaeYong-An <58469308+JaeYong-An@users.noreply.github.com>
2024-05-10 21:24:05 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,10 +1,8 @@
 class Solution {
     public String[] solution(String[] names) {
         String[] answer = new String[names.length/5+(names.length%5>0? 1:0)];
-        int idx = 0;
-        for(int i=0; i<names.length; i=i+5){
-            answer[idx]=names[i];
-            idx++;
+        for(int i=0; i<answer.length; i++){
+            answer[i]=names[i*5];
         }
         return answer;
     }
