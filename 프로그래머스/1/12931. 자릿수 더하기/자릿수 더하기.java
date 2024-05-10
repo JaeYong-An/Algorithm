a13e6c7111ce14e5be402226976f80a89701c3ec
JaeYong-An <58469308+JaeYong-An@users.noreply.github.com>
2024-05-10 18:42:54 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = n;
+        int d = 10;
+        while(num!=0){
+            answer+=num%d;
+            num=num/d;
+        }
+        return answer;
+    }
+}
