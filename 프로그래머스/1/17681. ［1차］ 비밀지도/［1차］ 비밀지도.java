ec3eb3cf5d68f21c762686e88de2b22a085b5357
JaeYong-An <58469308+JaeYong-An@users.noreply.github.com>
2024-06-18 21:08:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.17 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -6,24 +6,20 @@ class Solution {
             for(int j=n-1; j>=0; j--){
                 int bin = (int)Math.pow(2,j);
                 boolean trigger = false;
-                if(arr1[i]<bin && arr2[i]<bin){
-                    answer[i] += " ";
-                }else{
-                    if(arr1[i]/bin>=1){
+                if(arr1[i]/bin>=1){
+                    answer[i] += "#";
+                    arr1[i] -= bin;
+                    trigger = true;
+                }
+                if(arr2[i]/bin>=1){
+                    if(!trigger){
                         answer[i] += "#";
-                        arr1[i] -= bin;
                         trigger = true;
                     }
-                    if(arr2[i]/bin>=1){
-                        if(!trigger){
-                            answer[i] += "#";
-                            trigger = true;
-                        }
-                        arr2[i] -= bin;
-                    }
-                    if(!trigger){
-                        answer[i] += " ";
-                    }
+                    arr2[i] -= bin;
+                }
+                if(!trigger){
+                    answer[i] += " ";
                 }
             }
         }
