c0492fc78901b1b373f21668c28ab6c93e37f050
JaeYong-An <58469308+JaeYong-An@users.noreply.github.com>
2024-08-23 19:50:06 +0900

    [level 0] Title: 배열 만들기 6, Time: 37.03 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -14,8 +14,7 @@ class Solution {
         }
         int[] answer = new int[list.size()];
         if(answer.length==0){
-            answer = new int[1];
-            answer[0] = -1;
+            answer = new int[]{-1};
         }else{
             for(int i=0; i<answer.length; i++){
                 answer[i] = list.get(i);
