a403ac031243ce4a9aa0598e7acab26a0a32f439
JaeYong-An <58469308+JaeYong-An@users.noreply.github.com>
2025-02-06 22:40:42 +0900

    [level 0] Title: qr code, Time: 7.59 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        String str[] = code.split("");
+        for(int i=r; i<str.length; i=i+q){
+            answer+=str[i];
+        }
+        return answer;
+    }
+}
